28c998a0911c5210907ee0b9cd66741077897d06
Salvador Aldana <salvadortory88@gmail.com>
2023-10-28 17:18:18 -0600

    first commit

---
+++ Modulos/Academica/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
+from django.shortcuts import redirect
+
+def lanzador_admin(request):
+    return redirect('/admin/')
+
+
+
+# Create your views here.
+
+def formularioContacto(request):
+    return render(request, "formularioContacto.html")
+
+
+def contactar(request):
+    if request.method == "POST":
+        asunto = request.POST["txtAsunto"]
+        mensaje = request.POST["txtMensaje"] + " / Email: " + request.POST["txtMail"]
+        email_desde = settings.EMAIL_HOST_USER
+        email_para = ["salvialdana0@gmail.com"]
+        send_mail(asunto, mensaje, email_desde, email_para, fail_silently=False)
+        return render(request, "contactoExitoso.html")
+    return render(request, "formularioContacto.html")
---
+++ README.md
@@ -0,0 +1,9 @@
+# MiUniversidad
+Proyecto Django Para Administrar Universidad. Aplicación Web con Base de Datos y Envío de Correos.
+
+## Para utilizar:
+
+  * Ejecutar en la terminal, dentro de la carpeta del proyecto: ``python migrate.py runserver``
+  * En el navegador, acceder al /localhost o puerto 8000
+  * Para utilizar el administrador de Universidad, ingresar a /admin
+  
